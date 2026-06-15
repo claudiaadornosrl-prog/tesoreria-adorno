a22ed873ad2aa7b2198f08d68e378912bc0c4b20
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-06-15 18:25:10 -0300

    v19: tenencias FIMA (cuotapartes + valor unidad)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — Tesorería Adorno
-const CACHE_VERSION = 'tesoreria-adorno-v18-inversiones-drilldown';
+const CACHE_VERSION = 'tesoreria-adorno-v19-tenencias-fima';
 const CACHE_ASSETS = ['./', './index.html', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', e => {
