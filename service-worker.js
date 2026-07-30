42815e401103a008de352a5abd68285c70699095
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-07-30 12:04:51 -0300

    deploy 2026-07-30 12:04

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — Tesorería Adorno
-const CACHE_VERSION = 'tesoreria-adorno-v38-estrella-saldos';
+const CACHE_VERSION = 'tesoreria-adorno-v39-facturas';
 const CACHE_ASSETS = ['./', './index.html', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', e => {
