7971db874d3862fbb1292ffdb46f517f287e6ac7
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-07-30 14:03:50 -0300

    deploy 2026-07-30 14:03

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — Tesorería Adorno
-const CACHE_VERSION = 'tesoreria-adorno-v41-purga-facturas';
+const CACHE_VERSION = 'tesoreria-adorno-v42-editar-borrar-mov';
 const CACHE_ASSETS = ['./', './index.html', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', e => {
