20165db1866673fba34b15c8df6494966b54c4ec
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-08-11 15:04:01 -0300

    deploy 2026-08-11 15:04

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — Tesorería Adorno
-const CACHE_VERSION = 'tesoreria-adorno-v62-bots-pendientes-oficina';
+const CACHE_VERSION = 'tesoreria-adorno-v63-pendientes-mejoras';
 const CACHE_ASSETS = ['./', './index.html', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', e => {
