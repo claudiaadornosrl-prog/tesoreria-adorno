1acf70656f329ff42eb468a886f7e4622fcf06d5
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-09-10 08:49:52 -0300

    deploy 2026-09-10 08:49

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — Tesorería Adorno
-const CACHE_VERSION = 'tesoreria-adorno-v82-iconos-v2';
+const CACHE_VERSION = 'tesoreria-adorno-v83-sso';
 const CACHE_ASSETS = ['./', './index.html',
   './manual.js', './manifest.webmanifest', './favicon.svg', './fonts/URWGothic-Book.ttf'];
 
